371dc1d04dcd45977ac57007d2cee8bfaf3b27e9
Jamie Isaacs <pdt256@gmail.com>
2017-11-16 10:39:16 -0800

    Adding golang roman numberals.

---
+++ go/src/RomanNumerals/RomanNumerals.go
@@ -0,0 +1,11 @@
+package RomanNumerals
+
+type RomanNumeralConverter interface {
+	ConvertToRoman(i int) string
+}
+
+type RomanNumerals struct{}
+
+func (r RomanNumerals) ConvertToRoman(i int) (romanNumeral string) {
+	return
+}
