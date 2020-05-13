022095e39562470f64dda4ebaa381aa4fbcf947e
Jamie Isaacs <pdt256@gmail.com>
2020-05-13 15:32:16 -0700

    Moved timestring kata assertions into a single test

--- go/pkg/timestring/timestring_test.go
+++ go/pkg/timestring/timestring_test.go
@@ -11,10 +11,8 @@ import (
 func TestTimeStringSum(t *testing.T) {
 	assert.Equal(t, "0:0:0", timestring.SumOfTimes(""))
 	//assert.Equal(t, "0:0:1", timestring.SumOfTimes("0:1"))
-}
-
-func xTestAllTimeString(t *testing.T) {
-	assert.Equal(t, "0:0:0", timestring.SumOfTimes(""))
-	assert.Equal(t, "1:1:1", timestring.SumOfTimes("0:1 0:60 0:3600"))
-	assert.Equal(t, "2:32:41", timestring.SumOfTimes("12:32 34:01 15:23 9:27 55:22 25:56"))
+	//assert.Equal(t, "0:1:0", timestring.SumOfTimes("1:0"))
+	//assert.Equal(t, "1:0:0", timestring.SumOfTimes("60:0"))
+	//assert.Equal(t, "1:1:1", timestring.SumOfTimes("0:1 0:60 0:3600"))
+	//assert.Equal(t, "2:32:41", timestring.SumOfTimes("12:32 34:01 15:23 9:27 55:22 25:56"))
 }
